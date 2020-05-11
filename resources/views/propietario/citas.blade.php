7b6543174630b2f56b5dee673286b8376520d1a0
RomanCan <romancan33@gmail.com>
2020-05-11 10:04:49 -0400

    Revert "Revert "Revert "modificacion del modal de citas(cliente)"""
    This reverts commit 8b6ceacdd2d65b0ab1f1bb0b4c3af70d929f373b.

--- resources/views/propietario/citas.blade.php
+++ resources/views/propietario/citas.blade.php
@@ -52,7 +52,7 @@
                             </select>
                             <label >Descripción</label>
                             <input class="form-control" type="text" placeholder="Razón de la cita" v-model="descripcion"><br>
-                            <label>Fecha de la cita</label><input class="form-control" type="date" placeholder="Fecha de la cita" v-model="fecha_cita">
+                            <input class="form-control" type="date" placeholder="Fecha de la cita" v-model="fecha_cita">
                             
                         </div>
                         <div class="modal-footer">
